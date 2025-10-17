fe78ef672a204caa378df60194d5ac45c3011750
Konstantin Läufer <laufer@cs.luc.edu>
2025-10-16 19:32:13 -0500

    import of headless version of Android stopwatch with a stub for R.string
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

---
+++ src/main/java/edu/luc/etl/cs313/android/simplestopwatch/R.java
@@ -0,0 +1,14 @@
+package edu.luc.etl.cs313.android.simplestopwatch;
+
+public enum R {
+  ;
+
+  public enum string {
+    ;
+
+    public static final int STOPPED = 0;
+    public static final int RUNNING = 1;
+    public static final int LAP_STOPPED = 2;
+    public static final int LAP_RUNNING = 3;
+  }
+}
