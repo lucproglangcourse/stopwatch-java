7b7c8d2f14e3c4d7f38a0076a4d2eb505b2cc1c1
Konstantin Läufer <laufer@cs.luc.edu>
2025-10-20 22:42:59 -0500

    using Integer.valueOf to avoid hiding dependency by compile-time inlining
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/main/java/edu/luc/etl/cs313/android/simplestopwatch/R.java
+++ src/main/java/edu/luc/etl/cs313/android/simplestopwatch/R.java
@@ -1,10 +1,12 @@
 package edu.luc.etl.cs313.android.simplestopwatch;
 
-public interface R {
-  interface string {
-    int STOPPED = 0;
-    int RUNNING = 1;
-    int LAP_STOPPED = 2;
-    int LAP_RUNNING = 3;
+public class R {
+  public interface string {
+    // Using Integer.valueOf() prevents compile-time constant inlining
+    // This ensures ArchUnit can detect dependencies on this class
+    int STOPPED = Integer.valueOf(0);
+    int RUNNING = Integer.valueOf(1);
+    int LAP_STOPPED = Integer.valueOf(2);
+    int LAP_RUNNING = Integer.valueOf(3);
   }
 }
